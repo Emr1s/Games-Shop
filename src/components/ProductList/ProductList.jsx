7fc2a4dcb9af552b78353ac8a55699485b3d18d8
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-07-22 10:47:00 +0300

    add link to product page

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './ProductList.scss'
-import {useDispatch, useSelector} from 'react-redux'
-import {addCard} from '../../store/gameSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { addCard } from '../../store/gameSlice'
+import { Link } from 'react-router-dom'
 
 const ProductList = () => {
     const products = useSelector(state => state.game.game)
-    const cart = useSelector(state => state.game.cart)
     const dispatch = useDispatch()
 
     return (
         <div className="product-list">
             {products.map(item => (
                 <div className="item" key={item.id}>
-                    <img src={item.img} alt="" />
-                    <p>{item.name}</p>
-                    <p>Price: {item.price}$</p>
+                    <Link to={`/product/${item.id}`} className='link'>
+                        <img src={item.img} alt="" />
+                        <p>{item.name}</p>
+                        <p>Price: {item.price}$</p>
+                    </Link>
                     <div className="btn-buy">
                         <button onClick={() => dispatch(addCard(item))}>Buy</button>
                     </div>
-
                 </div>
             ))}
         </div>
