989b6287c522134293e70b58c84534cf6fd8ed69
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-07-24 16:00:42 +0300

    added sorting

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,25 +3,53 @@ import './ProductList.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { addCard, searchFilter, clearFilter } from '../../store/gameSlice'
 import { Link, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const ProductList = () => {
     const products = useSelector(state => state.game.game)
     const search = useSelector(state => state.game.search)
     const dispatch = useDispatch()
     const location = useLocation()
+    const [sort, setSort] = useState('all')
+
+    const sortProductsByPriceDesc = (a, b) => b.price - a.price;
+    const sortProductsByPriceAsc = (a, b) => a.price - b.price;
+    const sortProductsByNameAsc = (a, b) => a.name.localeCompare(b.name);
+    let sortedProducts = [...products]
+    if (sort === 'price-desc') {
+        sortedProducts.sort(sortProductsByPriceDesc)
+    } else if (sort === 'price-asc') {
+        sortedProducts.sort(sortProductsByPriceAsc)
+    } else if(sort === 'name-asc') {
+        sortedProducts.sort(sortProductsByNameAsc)
+    } else if(sort === 'all') {
+        sortedProducts = products
+    }
+
+
+    const sortHandler = e => {
+        setSort(e.target.value)
+    }
 
     useEffect(() => {
         dispatch(clearFilter())
-    },[location])
+    }, [location])
 
     return (
         <div className="list">
             <div className="center">
                 <input type="text" onChange={e => dispatch(searchFilter(e.target.value))} />
             </div>
+            <div className="sort">
+                <select value={sort} onChange={sortHandler} >
+                    <option value="all">All</option>
+                    <option value="price-desc">Sort by Price (High to Low)</option>
+                    <option value="price-asc">Sort by Price (Low to High)</option>
+                    <option value="name-asc">Sort by Name (A to Z)</option>
+                </select>
+            </div>
             <div className="product-list">
-                {products.filter(item => {
+                {sortedProducts.filter(item => {
                     if (search === '') {
                         return item
                     } else {
