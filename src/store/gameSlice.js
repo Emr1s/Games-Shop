10f5c7693b9982c7ba1ffb12c3674ddb7fca7afb
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-07-24 13:04:16 +0300

    added more games in store

--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -28,6 +28,30 @@ const gameSlice = createSlice({
                 img: "https://upload.wikimedia.org/wikipedia/ru/0/05/Diablo_III_cover.jpg",
             
             },
+            {
+                id: 3,
+                name: "Call of Duty: Modern Warfare® 2",
+                price: 30,
+                count: 1,
+                img: "https://upload.wikimedia.org/wikipedia/en/5/52/Call_of_Duty_Modern_Warfare_2_%282009%29_cover.png",
+            
+            },
+            {
+                id: 4,
+                name: "CS:GO",
+                price: 20,
+                count: 1,
+                img: "https://upload.wikimedia.org/wikipedia/uk/8/8c/Counter-Strike._Global_Offensive.jpg",
+            
+            },
+            {
+                id: 5,
+                name: "Heroes of might and magic",
+                price: 15,
+                count: 1,
+                img: "https://upload.wikimedia.org/wikipedia/uk/c/c7/HMM_III.jpg",
+            
+            }
         ],
         cart: [],
         search: "",
@@ -52,10 +76,12 @@ const gameSlice = createSlice({
         searchFilter: (state, action) => {
             state.search = action.payload
         },
-        
+        clearFilter: (state) => {
+            state.search = "";
+        }
     }
 })
 
-export const { addCard,removeCard,incCount, decCount, searchFilter } = gameSlice.actions;
+export const { addCard,removeCard,incCount, decCount, searchFilter, clearFilter } = gameSlice.actions;
 
 export default gameSlice.reducer;
